e3eba912304277f386789c94d02656bef9be6ff2
nakanoi <nakanoinryota@gmail.com>
2021-11-13 21:09:57 +0900

    Add get recipe requests.

--- get_category/main.py
+++ get_category/main.py
@@ -1,14 +1,20 @@
+import sys
+sys.path.append('.')
+sys.path.append('..')
+
 from datetime import datetime
 from request import get_category
 from set_record import recording
+from cur import Cur
 
 
 def main():
     print('Getting category started at {}.'.format(datetime.now()))
 
+    _cur = Cur()
     _category_dict, _json = get_category()
     if _category_dict and _json:
-        recording(_category_dict, _json)
+        recording(_cur, _category_dict, _json)
 
     print('Done at {}.'.format(datetime.now()))
 
--- get_category/script.sh
+++ get_category/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . /code/env.sh
-python /code/get_category/main.py >> /var/log/cron.log
+python /code/get_category/main.py >> /var/log/get_category_cron.log
